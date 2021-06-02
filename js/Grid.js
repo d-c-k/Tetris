60ba6c736c200e1474caa853aa4f3d938a49159d
d-c-k <dc.koefoed@gmail.com>
2021-06-02 14:59:02 +0200

    create Grid-class

---
+++ js/Grid.js
@@ -0,0 +1,22 @@
+class Grid {
+    constructor(width, height, parentNode) {
+        this.width = width
+        this.height = height
+        this.parentNode = parentNode
+    }
+
+    createGrid() {
+        const area = this.width*this.height
+        for(let i = 1; i <= area; i++) {
+            let cell = document.createElement('div')
+            this.parentNode.appendChild(cell)
+        }
+    }
+
+    createFloor() {
+        for(let i = 1; i <= this.width; i++) {
+            let floorCell = document.createElement('div')
+            this.parentNode.appendChild(floorCell)
+        }
+    }
+}
